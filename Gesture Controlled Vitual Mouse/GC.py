033ca6bc845aa84f0403152b074c2f215d606441
Niteesh011 <147575131+Niteesh011@users.noreply.github.com>
2024-08-24 16:27:44 +0530

    Update GC.py

--- Gesture Controlled Vitual Mouse/GC.py
+++ Gesture Controlled Vitual Mouse/GC.py
@@ -9,11 +9,6 @@ from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 from google.protobuf.json_format import MessageToDict
 import screen_brightness_control as sbcontrol
 
-
-
-# To create GUI
-import tkinter as tk
-from PIL import ImageTk, Image
 pyautogui.FAILSAFE = False
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
@@ -416,19 +411,5 @@ class GestureController:
 
 
 # uncomment to run directly
-def runvirtualmouse():
-    gc1 = GestureController()
-    gc1.start()
-root = tk.Tk()
-root.geometry("300x300")
-label = tk.Label(root, text="Welcome to Virtual Mouse", fg="brown",font='TkDefaultFont 16 bold')
-label.grid(row=0, columnspan=5, pady=10, padx=10)
-image = ImageTk.PhotoImage(Image.open("tap.png"))
-img_label = tk.Label(image=image , width=100, height=100, borderwidth=3, relief="solid")
-img_label.grid(row=1, columnspan=5, pady=10, padx=10)
-start_button = tk.Button(root,text=" Track Mouse",fg="white", bg='green', font='Helvetica 12 bold italic ',command= runvirtualmouse , height="4", width="16",activebackground='lightblue')
-start_button.grid(row=3,column=2, pady=10, padx=20)
-root.mainloop()
-label.geometery("400X300")
-root.geometery(row =0,columnspan=5, pady= 10,padx=10)
-root.main.loop()
+gc1 = GestureController()
+gc1.start()
